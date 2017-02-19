4dd6f9f98d2b35fe57ccdd273004877989d09076
Antti Salmivaara <antti.salmivaara@iki.fi>
2017-02-19 02:12:52 +0200

    Configuring project

---
+++ target/main.out/boot/cljs/main299.cljs
@@ -0,0 +1,1 @@
+(ns boot.cljs.main299 (:require master-and-apprentice-3.core))
---
+++ target/main.out/boot/cljs/main299.js
@@ -0,0 +1,6 @@
+// Compiled by ClojureScript 1.7.170 {}
+goog.provide('boot.cljs.main299');
+goog.require('cljs.core');
+goog.require('master_and_apprentice_3.core');
+
+//# sourceMappingURL=main299.js.map
